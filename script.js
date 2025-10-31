1fd862dbdac59a2eaa061051e0ff03aa1a3b80d1
DanteM-max <DanteMatanza1301@gmail.com>
2025-10-31 22:04:05 +0000

    Style fixed! Need to fix JS

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ function addTask() {
 
     //Get task text!
     const textbox = document.getElementById("task-input");
-    
     let taskText = textbox.value;
+    if (taskText == "") {
+        alert("Tasks cannot be empty!");
+    }
     textbox.value = "";
 
     //get taskList
@@ -28,23 +30,22 @@ function addTask() {
         taskDiv.style.backgroundColor = "darkblue";
     }
     taskDiv.classList.add("task-item");
+
     //make input (checkbox)
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = "checkbox" + idNum;
+
     //make label
     let label = document.createElement("label");
     label.id = "label" + idNum;
+
     //set label text to user's input
     label.innerText = taskText;
+
     //append new elements (checkbox to task) (label to task) (task to list)
-
-        //checkbox
-
     taskDiv.appendChild(checkbox);
-
     taskDiv.appendChild(label);
-
     taskList.appendChild(taskDiv);
     console.log("Code complete!");
 }
